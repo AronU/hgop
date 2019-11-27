e6fe0833180ab1c688570acf0f48a6a18bff2de8
AronU <aronu17@ru.is>
2019-11-27 14:13:19 +0000

    Added initialize_game_api_instance script and modified infrastructure.tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,8 +1,10 @@
+# TODO Comment 2-3 sentences.
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
+# TODO Comment 2-3 sentences.
 resource "aws_security_group" "game_security_group" {
   name = "GameSecurityGroup"
 
@@ -28,6 +30,7 @@ resource "aws_security_group" "game_security_group" {
   }
 }
 
+# TODO Comment 2-3 sentences.
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -37,14 +40,41 @@ resource "aws_instance" "game_server" {
     Name = "GameServer"
   }
 
+  # TODO Comment 1-2 sentences.
+  provisioner "file" {
+    source      = "scripts/initialize_game_api_instance.sh"
+    destination = "/home/ubuntu/initialize_game_api_instance.sh"
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+
+  # TODO Comment 1-2 sentences.
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yml"
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+
   # This is used to run commands on the instance we just created.
   # Terraform does this by SSHing into the instance and then executing the commands.
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
+  # TODO Comment 1-2 sentences.
   provisioner "remote-exec" {
     inline = [
-      "# Managed to SSH into instance",
+      "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
     ]
 
     connection {
@@ -56,6 +86,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
+# TODO Comment 1-2 sentences.
 output "public_ip" {
   value = aws_instance.game_server.public_ip
 }
