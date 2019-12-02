a13fd3dc56c8a8782418a9dccebd2bb9f87a3895
Andri <andri.lorenz@gmail.com>
2019-12-02 14:39:24 +0000

    docker_build.sh add it

---
+++ scripts/docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# with this || We can abandon the command if anything is wrong and at the same time stade where things went wrong
+GIT_COMMIT=$1 || { echo 'Commit failed' ; exit 1; }
+
+docker build -t username/repo:$GIT_COMMIT item_repository/ || { echo 'docker build failed' ; exit 1; }
