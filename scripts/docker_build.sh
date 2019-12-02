de4b97c59dde6e2c45214879f0ae47293aebc2b4
Andri <andri.lorenz@gmail.com>
2019-12-02 14:57:31 +0000

    Conectid to docker

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,4 +2,4 @@
 # with this || We can abandon the command if anything is wrong and at the same time stade where things went wrong
 GIT_COMMIT=$1 || { echo 'Commit failed' ; exit 1; }
 
-docker build -t username/repo:$GIT_COMMIT item_repository/ || { echo 'docker build failed' ; exit 1; }
+docker build -t andrilor/hgop:$GIT_COMMIT item_repository/ || { echo 'docker build failed' ; exit 1; }
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,4 +2,4 @@
 
 GIT_COMMIT=$1|| { echo 'Commit failed' ; exit 1; }
 #Docker push. Will exit if it fails. 
-docker push username/repo:$GIT_COMMIT || { echo 'docker push failed' ; exit 1; }
+docker push andrilor/hgop:$GIT_COMMIT || { echo 'docker push failed' ; exit 1; }
