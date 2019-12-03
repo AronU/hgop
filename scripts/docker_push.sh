b3a32c5ac2dbbac65445e10f833f09b793b3eb47
Andri <andri.lorenz@gmail.com>
2019-12-03 12:27:24 +0000

    index error fix

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-GIT_COMMIT=$1|| { echo 'Commit failed' ; exit 1; }
+GIT_COMMIT=$1 || { echo 'Commit failed' ; exit 1; }
 #Docker push. Will exit if it fails. 
 docker push andrilor/jenkins:$GIT_COMMIT || { echo 'docker push failed' ; exit 1; }
