a43ce3b45a1faeb606daf571f819a1714255f1fe
Andri <andri.lorenz@gmail.com>
2019-12-03 13:45:56 +0000

    name change on dir

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,4 +2,4 @@
 # with this || We can abandon the command if anything is wrong and at the same time stade where things went wrong
 GIT_COMMIT=$1 || { echo 'Commit failed' ; exit 1; }
 
-docker build -t andrilor/jenkins:$GIT_COMMIT item_repository/ || { echo 'docker build failed' ; exit 1; }
+docker build -t andrilor/jenkins:$GIT_COMMIT game_api/ || { echo 'docker build failed' ; exit 1; }
