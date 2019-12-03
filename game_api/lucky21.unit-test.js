c3486aa0f6d425c36834b33e888681b6dc9caa8a
AronU <aronu17@ru.is>
2019-12-03 15:04:03 +0000

    New test added. Old need updating

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -1,4 +1,6 @@
 const lucky21Constructor = require('./lucky21.js');
+const deckConstructor = require('./deck.js');
+const dealerConstructor = require('./dealer.js');
 
 test('a new game should have 50 cards left in the deck', () => {
   let game = lucky21Constructor();
@@ -8,4 +10,25 @@ test('a new game should have 50 cards left in the deck', () => {
 test('a new game should have 2 drawn cards', () => {
   let game = lucky21Constructor();
   expect(game.state.cards.length).toEqual(2);
+});
+
+test('guess21OrUnder should draw the next card', () => {
+  // Arrange
+  let deck = deckConstructor();
+  deck = [
+      '05C', '01D', '09S', '10H', 
+  ];
+  let dealer = dealerConstructor();
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+  
+  // Inject our dependencies
+  let game = lucky21Constructor(deck, dealer);
+  
+  // Act
+  game.guess21OrUnder(game);
+  
+  // Assert
+  expect(game.state.cards.length).toEqual(3);
+  expect(game.state.cards[2]).toEqual('01D');
 });
