720ebb6eca3bd4859f72ee9e941b037d14f4aca7
Andri <andri.lorenz@gmail.com>
2019-12-04 21:16:27 +0000

    URL add it

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JENKINS_URL=# YOUR JENKINS URL
+JENKINS_URL=http://54.152.217.158:8080
 
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
