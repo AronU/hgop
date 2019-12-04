6a690c49963b2f5cd45620597cdbd5ba2624798c
Andri <andri.lorenz@gmail.com>
2019-12-04 21:12:01 +0000

    working on it

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -24,10 +24,8 @@ terraform apply -auto-approve
 echo "Game API running at " + $(terraform output public_ip)
 
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "chmod +x docker_compose_up.sh"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
 
-sleep 40s
 curl ubuntu$(terraform output public_ip):3000/status || exit 1
 
 exit 0
