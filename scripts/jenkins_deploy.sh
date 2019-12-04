8f8a16cac1fb19f0207c11883000f6b94632d6e5
Andri <andri.lorenz@gmail.com>
2019-12-04 20:57:51 +0000

    jenkins_deploy DOTO done?

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -12,8 +12,8 @@ cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scrip
 rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
 cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
 
-# TODO: Delete all .tf files from /var/lib/jenkins/terraform/hgop/production
-# TODO: Copy all .tf files from repository to /var/lib/jenkins/terraform/hgop/production
+rm -f /var/lib/jenkins/terraform/hgop/production/*.tf
+cp *.tf /var/lib/jenkins/terraform/hgop/production
 
 cd /var/lib/jenkins/terraform/hgop/production
 terraform init # In case terraform is not initialized.
@@ -25,6 +25,7 @@ echo "Game API running at " + $(terraform output public_ip)
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
 
-#TODO exit on error if deployment fails.
+
+curl ubuntu$(terraform output public_ip):3000/status
 
 exit 0
