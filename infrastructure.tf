cfa22c0c9c4a0e82fe53c5c3f45a41e486952560
Andri <andri.lorenz@gmail.com>
2019-12-05 15:25:24 +0000

    infrastructure.tf change

--- infrastructure.tf
+++ infrastructure.tf
@@ -89,6 +89,51 @@ resource "aws_instance" "game_server" {
   }
 }
 
+# instance_type is what kind of instance we use at AWS. t2.micro is the free version. Security
+# key is "GameKeyPair".
+resource "aws_instance" "game_server" {
+  ami                    = "ami-0ac019f4fcb7cb7e6"
+  instance_type          = "t2.micro"
+  key_name               = "GameKeyPair"
+  vpc_security_group_ids = [aws_security_group.game_security_group.id]
+  tags = {
+    Name = "GameServer"
+  }
+
+  # docker_compose_up.sh script installs everything required, like Docker and Docker-
+  # Compose. It sends it to the new AWS instance.
+  provisioner "file" {
+    source      = "scripts/docker_compose_up.sh"
+    destination = "/home/ubuntu/docker_compose_up.sh"
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+
+  # This is used to run commands on the instance we just created.
+  # Terraform does this by SSHing into the instance and then executing the commands.
+  # Since it can take time for the SSH agent on machine to start up we let Terraform
+  # handle the retry logic, it will try to connect to the agent until it is available
+  # that way we know the instance is available through SSH after Terraform finishes.
+
+  # Changes privilege of docker_compose_up.sh script. Makes it excecutable.
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/docker_compose_up.sh",
+    ]
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+}
 # Gets the public IP and saves it as a new instance. Displays it in terminal as well.
 output "public_ip" {
   value = aws_instance.game_server.public_ip
