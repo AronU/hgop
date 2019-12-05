690282bbb0714d6d926ad9b71d4bf6d5c0d494cd
Andri <andri.lorenz@gmail.com>
2019-12-05 14:39:07 +0000

    sunc_session change and pipline shoud work now

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -3,7 +3,7 @@
 JENKINS_URL=ec2-54-152-217-158.compute-1.amazonaws.com
 
 scp -o StrictHostKeyChecking=no -i "~/Documents/Jenkin.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
-ssh -o StrictHostKeyChecking=no -i "~/Documents/Jenkin.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
+ssh -o StrictHostKeyChecking=no -i "~/Documents/Jenkin.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws"
 ssh -o StrictHostKeyChecking=no -i "~/Documents/Jenkin.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
 
 scp -o StrictHostKeyChecking=no -i "~/Documents/Jenkin.pem" ~/.aws/GameKeyPair.pem ubuntu@${JENKINS_URL}:~
