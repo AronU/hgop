367dcc12e2607744cb55fd32dd3b24fa71b73e57
Andri <andri.lorenz@gmail.com>
2019-12-05 19:38:51 +0000

    fix

--- infrastructure.tf
+++ infrastructure.tf
@@ -54,7 +54,19 @@ resource "aws_instance" "game_server" {
       private_key = file("~/.aws/GameKeyPair.pem")
     }
   }
+  # docker_compose_up.sh script installs everything required, like Docker and Docker-
+  # Compose. It sends it to the new AWS instance.
+  provisioner "file" {
+    source      = "scripts/docker_compose_up.sh"
+    destination = "/home/ubuntu/docker_compose_up.sh"
 
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
   # Sends the Docker-compose.yml file to the new AWS instance so it can be accessed later.
   provisioner "file" {
     source      = "docker-compose.yml"
@@ -89,23 +101,6 @@ resource "aws_instance" "game_server" {
   }
   # docker_compose_up.sh script installs everything required, like Docker and Docker-
   # Compose. It sends it to the new AWS instance.
-  provisioner "file" {
-    source      = "scripts/docker_compose_up.sh"
-    destination = "/home/ubuntu/docker_compose_up.sh"
-
-    connection {
-      host        = coalesce(self.public_ip, self.private_ip)
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.aws/GameKeyPair.pem")
-    }
-  }
-
-  # This is used to run commands on the instance we just created.
-  # Terraform does this by SSHing into the instance and then executing the commands.
-  # Since it can take time for the SSH agent on machine to start up we let Terraform
-  # handle the retry logic, it will try to connect to the agent until it is available
-  # that way we know the instance is available through SSH after Terraform finishes.
 
   # Changes privilege of docker_compose_up.sh script. Makes it excecutable.
   provisioner "remote-exec" {
